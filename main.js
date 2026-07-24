d3aa4080e02f8e2eaa60d2a124b834a60fd55759
destinygc <efootballacct@hotmail.com>
2026-07-24 21:18:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const API_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:3000/api'
-    : `https://backend-v32l.onrender.com`;  
+    : `https://backend-v32l.onrender.com/api`;  
 
 // Wait for the HTML to fully load
 document.addEventListener("DOMContentLoaded", function () {
